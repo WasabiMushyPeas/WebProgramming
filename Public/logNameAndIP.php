6240a4331b5685b7e41ae0a47ad107bf69bc3a8e
Jackson McDowell <Jackson.McDowell@gmail.com>
2024-01-16 16:28:24 -0700

    Refactor logNameAndIP.php to improve code readability and handle unknown names

--- Public/logNameAndIP.php
+++ Public/logNameAndIP.php
@@ -1,67 +1,75 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/images/php.png">
     <title>PHP</title>
 </head>
+
 <body>
 
-    Name: <?php
+    Name:
+    <?php
     // Check if name is set
     if (isset($_POST["name"])) {
         echo $_POST["name"];
-    }else {
+    } else {
         echo "No name set";
     }
-    echo 
     ?><br>
 
 
-    IP: <?php 
+    IP:
+    <?php
     // Check if ip is set
     if (isset($_SERVER['REMOTE_ADDR'])) {
         echo $_SERVER['REMOTE_ADDR'];
-    }else {
+    } else {
         echo "No ip set";
     }
     ?><br>
 
     <?php
+    if (isset($_POST["name"])) {
         $name = $_POST["name"];
-        $ip = $_SERVER['REMOTE_ADDR'];
-        $file = fopen("log.txt", "a");
-        fwrite($file, $name . " | " . $ip . "\n");
-        fclose($file);
+    } else {
+        $name = "UNKNOWN";
+    }
+    $ip = $_SERVER['REMOTE_ADDR'];
+    $file = fopen("log.txt", "a");
+    fwrite($file, $name . " | " . $ip . "\n");
+    fclose($file);
 
-        // make a file for each user
-        $users = fopen("users.txt", "r+");
+    // make a file for each user
+    $users = fopen("users.txt", "r+");
 
-        // check if user exists
-        $userExists = false;
-        while(!feof($users)) {
-            $line = fgets($users);
-            $line = explode(" | ", $line);
-            if ($line[0] == $name) {
-                $userExists = true;
-            }
+    // check if user exists
+    $userExists = false;
+    while (!feof($users)) {
+        $line = fgets($users);
+        $line = explode(" | ", $line);
+        if ($line[0] == $name && $line[0] != "UNKNOWN") {
+            $userExists = true;
         }
-        // check if ip exists
-        while(!feof($users)) {
-            $line = fgets($users);
-            $line = explode(" | ", $line);
-            if ($line[1] == $ip) {
-                $userExists = true;
-            }
+    }
+    // check if ip exists
+    while (!feof($users)) {
+        $line = fgets($users);
+        $line = explode(" | ", $line);
+        if ($line[1] == $ip) {
+            $userExists = true;
         }
-        if (!$userExists) {
-            fwrite($users, $name . " | " . $ip . "\n");
-        }else {
-            echo "Welcome back!";
-        }
-        fclose($users);
+    }
+    if (!$userExists) {
+        fwrite($users, $name . " | " . $ip . "\n");
+    } else {
+        echo "Welcome back!";
+    }
+    fclose($users);
     ?>
 
-</form>
+    </form>
 </body>
+
 </html>
