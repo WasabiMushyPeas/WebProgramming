6f9370e8a63bd0798825aeb9108d1f919d798008
Jackson McDowell <Jackson.McDowell@gmail.com>
2024-01-18 15:51:28 -0700

    Add black rectangle to canvas

--- HTML/Paint/paint.js
+++ HTML/Paint/paint.js
@@ -3,4 +3,6 @@ let canvas;
 function start(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
+    ctx.fillStyle = "black";
+    ctx.fillRect(10, 30, 50, 100);
 }
