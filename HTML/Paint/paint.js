9e5a6efe5230e9d280d78068b000e3d0896c9e8c
Jackson McDowell <Jackson.McDowell@gmail.com>
2024-01-18 16:24:04 -0700

    Add mouseDown event handler to draw red squares

--- HTML/Paint/paint.js
+++ HTML/Paint/paint.js
@@ -1,8 +1,14 @@
 let canvas;
+let ctx;
 
 function start(){
     canvas = document.getElementById("canvas");
     ctx = canvas.getContext("2d");
     ctx.fillStyle = "black";
     ctx.fillRect(10, 30, 50, 100);
+}
+
+function mouseDown(event){
+    ctx.fillStyle = "red";
+    ctx.fillRect(event.offsetX, event.offsetY, 10, 10);
 }
