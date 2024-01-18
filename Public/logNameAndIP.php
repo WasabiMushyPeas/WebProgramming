6201f3ec1fa010b5b6d5719de43c97d2f5d475f2
Jackson McDowell <Jackson.McDowell@gmail.com>
2024-01-18 15:44:07 -0700

    Update file paths and remove unused files

--- Public/logNameAndIP.php
+++ Public/logNameAndIP.php
@@ -38,12 +38,12 @@
         $name = "UNKNOWN";
     }
     $ip = $_SERVER['REMOTE_ADDR'];
-    $file = fopen("log.txt", "a");
+    $file = fopen("./LOGS/log.txt", "a");
     fwrite($file, $name . " | " . $ip . "\n");
     fclose($file);
 
     // make a file for each user
-    $users = fopen("users.txt", "r+");
+    $users = fopen("./LOGS/users.txt", "r+");
 
     // check if user exists
     $userExists = false;
