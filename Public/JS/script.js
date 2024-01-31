cdabd1002b306494db1fdeafc584d6a804250f26
Jackson McDowell <Jackson.McDowell@gmail.com>
2024-01-30 17:05:01 -0700

    Refactor page reload function

--- Public/JS/script.js
+++ Public/JS/script.js
@@ -1,6 +1,6 @@
 // Function that reloads the page every 10 seconds
-function onStart(){
-    setInterval(function(){
-        location.reload();
-    }, 10000);
-}
+// function onStart(){
+//     setInterval(function(){
+//         location.reload();
+//     }, 10000);
+// }
