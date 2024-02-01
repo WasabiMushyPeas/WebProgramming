160e258340673d397ca45d38fca7a3eea8d03131
Jackson McDowell <Jackson.McDowell@gmail.com>
2024-02-01 15:48:36 -0700

    Add favicon to user.php

--- Public/user.php
+++ Public/user.php
@@ -32,6 +32,7 @@ if (isset($_COOKIE['mode']) && $_COOKIE['mode'] == 'dark') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="icon" type="image/x-icon" href="./IMAGES/poster.png">
     <title>Create Post</title>
 </head>
 
